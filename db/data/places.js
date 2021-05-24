048c9f1342f2a7134baecb632bbdc650803b320b
Ornella <ornellaspada@gmail.com>
2021-05-24 18:35:11 +0200

    db new places

--- db/data/places.js
+++ db/data/places.js
@@ -8,7 +8,7 @@ export default [
     lat: 2,
     long: 2,
     image: 'https',
-    catagories: [
+    categories: [
       'skate',
       'park'
     ],
@@ -23,10 +23,89 @@ export default [
     lat: 21,
     long: 21,
     image: 'https',
-    catagories: [
+    categories: [
       'skate',
       'park'
     ],
     rating: 5,
+  },
+  {
+    name: 'Sir John Soane Museum',
+    description: 'Sir John Soane Museum is a house museum,formerly the home of neo-classical architect, John Soane. It holds many drawings and architectural models of Soane\'s projects, and a large collection of paintings, sculptures, drawings and antiquities that he acquired over many years.',
+    area: 'Holborn',
+    address: '13 Lincoln\'s Inn Fields',
+    postcode: 'WC2A 3BP',
+    lat: 21,
+    long: 21,
+    image: ' https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNjhdFm2Kqj5ILPud6oL5L_y8-z-yR1E-gGw&usqp=CAU',
+    categories: [
+      'museum',
+      'art'
+    ],
+    rating: 5, 
+  },
+  {
+    name: 'Ada Salter Rose Garden',
+    description: 'There is a lovely rose garden, a duck pond, two cafes, tennis courts, a bandstand, outdoor gym, a bandstand and four toilets.',
+    area: 'Rotherhithe',
+    address: 'Southwalk Park',
+    postcode: 'SE1 0AB',
+    lat: 21,
+    long: 21,
+    image: ' https://upload.wikimedia.org/wikipedia/commons/f/f8/Ada_Salter_Rose_Garden._Southwark_Park%2C_Rotherhithe%2C_London%2C_SE16_-_geograph.org.uk_-_1516940.jpg ',
+    categories: [
+      'garden',
+      'nature'
+    ],
+    rating: 5, 
+  }, 
+  {
+    name: 'Claridge Hotel ',
+    description: 'There is something magical about a Claridge’s room or suite. As well as being among the most spacious in London, each one has been beautifully designed and furnished, complete with many thoughtful, luxurious touches.',
+    area: 'Mayfair',
+    address: 'Brook St, London ',
+    postcode: 'w1k 4hr',
+    lat: 21,
+    long: 21,
+    image: ' ',
+    categories: [
+      'hotel',
+      'leisure',
+      'accomodation'
+    ],
+    rating: 5, 
+  }, 
+  {
+    name: 'Chelsea Physics Garden',
+    description: 'For Londoners, this garden is nestled in a microclimate next to the Thames on Swan Walk in Chelsea. The walled space was founded in 1673 by the Worshipful Society of Apothecaries for apprentices to study medicinal plants. It’s also the oldest botanical garden in London. Recently the garden secured funding for its heritage project to restore its historic range of Victorian-designed glasshouses, with work set to begin in spring this year.',
+    area: 'Chelsea',
+    address: '66 Royal Hospital Road',
+    postcode: 'SW3 4HS',
+    lat: 21,
+    long: 21,
+    image: ' ',
+    categories: [
+      'garden',
+      'nature'
+    ],
+    rating: 5, 
+  }, 
+  {
+    name: 'Chelsea Physics ',
+    description: 'For Londoners, this garden is nestled in a microclimate next to the Thames on Swan Walk in Chelsea. The walled space was founded in 1673 by the Worshipful Society of Apothecaries for apprentices to study medicinal plants. It’s also the oldest botanical garden in London. Recently the garden secured funding for its heritage project to restore its historic range of Victorian-designed glasshouses, with work set to begin in spring this year.',
+    area: 'Chelsea',
+    address: '66 Royal Hospital Road',
+    postcode: 'SW3 4HS',
+    lat: 21,
+    long: 21,
+    image: ' ',
+    categories: [
+      'garden',
+      'nature'
+    ],
+    rating: 5, 
   }
+
+
+
 ]
